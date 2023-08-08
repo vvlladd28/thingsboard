16ae2f100a446e7446c4b13e57a7758ef38e6424
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2023-08-08 11:04:16 +0300

    UI: Entities table rewrite textSearch in control

--- ui-ngx/src/app/modules/home/components/entity/entities-table.component.ts
+++ ui-ngx/src/app/modules/home/components/entity/entities-table.component.ts
@@ -38,7 +38,7 @@ import { MatSort, SortDirection } from '@angular/material/sort';
 import { EntitiesDataSource } from '@home/models/datasource/entity-datasource';
 import { catchError, debounceTime, distinctUntilChanged, map, skip, startWith, tap } from 'rxjs/operators';
 import { Direction, SortOrder } from '@shared/models/page/sort-order';
-import { forkJoin, fromEvent, merge, Observable, of, Subscription } from 'rxjs';
+import { forkJoin, merge, Observable, of, Subscription } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { BaseData, HasId } from '@shared/models/base-data';
 import { ActivatedRoute, QueryParamsHandling, Router } from '@angular/router';
@@ -59,12 +59,13 @@ import { AddEntityDialogData, EntityAction } from '@home/models/entity/entity-co
 import { calculateIntervalStartEndTime, HistoryWindowType, Timewindow } from '@shared/models/time/time.models';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { TbAnchorComponent } from '@shared/components/tb-anchor.component';
-import { isDefined, isEmptyStr, isEqual, isString, isUndefined } from '@core/utils';
+import { isDefined, isEmptyStr, isEqual, isNotEmptyStr, isUndefined } from '@core/utils';
 import { HasUUID } from '@shared/models/id/has-uuid';
 import { ResizeObserver } from '@juggle/resize-observer';
 import { hidePageSizePixelValue } from '@shared/models/constants';
 import { EntitiesTableAction, IEntitiesTableComponent } from '@home/models/entity/entity-table-component.models';
 import { EntityDetailsPanelComponent } from '@home/components/entity/entity-details-panel.component';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'tb-entities-table',
@@ -128,16 +129,7 @@ export class EntitiesTableComponent extends PageComponent implements IEntitiesTa
 
   private rxSubscriptions = new Array<Subscription>();
 
-  private _textSearch: string | null;
-
-  get textSearch(): string | null {
-    return this._textSearch;
-  }
-
-  set textSearch(value: string | null) {
-    this.pageLink.textSearch = value !== null ? value.trim() : null;
-    this._textSearch = value;
-  }
+  textSearch = this.fb.control('', {nonNullable: true});
 
   constructor(protected store: Store<AppState>,
               public route: ActivatedRoute,
@@ -148,7 +140,8 @@ export class EntitiesTableComponent extends PageComponent implements IEntitiesTa
               private cd: ChangeDetectorRef,
               private router: Router,
               private componentFactoryResolver: ComponentFactoryResolver,
-              private elementRef: ElementRef) {
+              private elementRef: ElementRef,
+              private fb: FormBuilder) {
     super(store);
   }
 
@@ -279,9 +272,12 @@ export class EntitiesTableComponent extends PageComponent implements IEntitiesTa
       if (routerQueryParams.hasOwnProperty('pageSize')) {
         this.pageLink.pageSize = Number(routerQueryParams.pageSize);
       }
-      if (routerQueryParams.hasOwnProperty('textSearch') && !isEmptyStr(routerQueryParams.textSearch)) {
+      const textSearchParam = routerQueryParams.textSearch;
+      if (textSearchParam && !isEmptyStr(textSearchParam)) {
+        const decodedTextSearch = decodeURI(routerQueryParams.textSearch);
         this.textSearchMode = true;
-        this.textSearch = decodeURI(routerQueryParams.textSearch);
+        this.textSearch.setValue(decodedTextSearch, { emitEvent: false });
+        this.pageLink.textSearch = decodedTextSearch.trim();
       }
     }
     this.dataSource = this.entitiesTableConfig.dataSource(this.dataLoaded.bind(this));
@@ -316,25 +312,26 @@ export class EntitiesTableComponent extends PageComponent implements IEntitiesTa
 
   ngAfterViewInit() {
 
-    fromEvent(this.searchInputField.nativeElement, 'keyup')
-      .pipe(
+    this.textSearch.valueChanges.pipe(
         debounceTime(150),
-        map(() => this.textSearch),
         startWith(''),
         distinctUntilChanged((a: string, b: string) => a.trim() === b.trim()),
-        skip(1),
-        tap(() => {
-          const queryParams: PageQueryParam = {
-            textSearch: isString(this.textSearch) && this.textSearch !== '' ? encodeURI(this.textSearch) : null
-          };
-          if (this.displayPagination) {
-            this.paginator.pageIndex = 0;
-            queryParams.page = null;
-          }
-          this.updatedRouterParamsAndData(queryParams);
-        })
-      )
-      .subscribe();
+        skip(1)
+    ).subscribe(value => {
+      const queryParams: PageQueryParam = {};
+      if (isNotEmptyStr(value)) {
+        queryParams.textSearch = encodeURI(value);
+        this.pageLink.textSearch = value.trim();
+      } else {
+        queryParams.textSearch = null;
+        this.pageLink.textSearch = null;
+      }
+      if (this.displayPagination) {
+        this.paginator.pageIndex = 0;
+        queryParams.page = null;
+      }
+      this.updatedRouterParamsAndData(queryParams);
+    });
 
     if (this.pageMode) {
       this.route.queryParams.pipe(skip(1)).subscribe((params: PageQueryParam) => {
@@ -580,7 +577,6 @@ export class EntitiesTableComponent extends PageComponent implements IEntitiesTa
 
   enterFilterMode() {
     this.textSearchMode = true;
-    this.textSearch = '';
     setTimeout(() => {
       this.searchInputField.nativeElement.focus();
       this.searchInputField.nativeElement.setSelectionRange(0, 0);
@@ -589,15 +585,7 @@ export class EntitiesTableComponent extends PageComponent implements IEntitiesTa
 
   exitFilterMode() {
     this.textSearchMode = false;
-    this.textSearch = null;
-    const queryParams: PageQueryParam = {
-      textSearch: null
-    };
-    if (this.displayPagination) {
-      this.paginator.pageIndex = 0;
-      queryParams.page = null;
-    }
-    this.updatedRouterParamsAndData(queryParams);
+    this.textSearch.reset();
   }
 
   resetSortAndFilter(update: boolean = true, preserveTimewindow: boolean = false) {
